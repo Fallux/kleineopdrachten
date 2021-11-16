877f04bf9db97ffbe908d6cb62dce803e3564112
Roemer Naber <200611@student.glu.nl>
2021-11-16 16:43:58 +0100

    intro opdracht1

---
+++ task1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" id="inputT" name="text" placeholder="Tekst">
+        <input type="submit" value="submit" name="submit">
+        <div id="output">Hier moet tekst komen <br> </br>
+        <?php
+        
+        ?>
+
+
+
+
+
+        </div>
+    </form>
+</body>
+</html>
